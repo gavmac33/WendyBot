7b7afe9f1fa37a543098b99f00dbad56a850f8f6
gavmac33 <gavin.m.macpherson@vanderbilt.edu>
2019-12-24 15:08:05 -0500

    Added announcement code

---
+++ messager/main.py
@@ -0,0 +1,27 @@
+from twilio.rest import Client
+import firebase_admin
+from firebase_admin import firestore
+import os
+import base64
+
+def env_vars(var):
+    return os.environ.get(var, 'Specified environment variable is not set.')
+
+# Define global constants using environment variables
+ACCOUNT_SID = env_vars("ACCOUNT_SID")
+AUTH_TOKEN = env_vars("AUTH_TOKEN")
+WARRIOR_BOT_NUMBER = env_vars("WARRIOR_BOT_NUMBER")
+WENDYS_NUMBER = env_vars("WENDYS_NUMBER")
+SMS_CLIENT = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+
+def message(event, context):
+    message = base64.b64decode(event['data']).decode('utf-8')
+
+
+    SMS_CLIENT.messages.create(
+        to=WENDYS_NUMBER,
+        from_=WARRIOR_BOT_NUMBER,
+        body="WendysWarriorsBot- " + message
+    )
+
